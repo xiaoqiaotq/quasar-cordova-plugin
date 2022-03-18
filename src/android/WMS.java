56b9f3fd5dec6321353d419b63a28528c2b17e01
CI25290 <TonyWang1@careerintlinc.com>
2022-03-19 00:20:47 +0800

    add pda

--- src/android/WMS.java
+++ src/android/WMS.java
@@ -1,5 +1,13 @@
 package org.apache.cordova.wms;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Message;
+import android.util.Log;
 import android.widget.Toast;
 
 import org.apache.cordova.CordovaPlugin;
@@ -16,6 +24,36 @@ import java.util.UUID;
  * This class echoes a string called from JavaScript.
  */
 public class WMS extends CordovaPlugin {
+  private static final String TAG = "WMS_PLUGIN";
+  public static final String ACTION_DECODE = "android.intent.ACTION_DECODE_DATA";
+  public static final String BARCODE_STRING_TAG = "barcode_string";
+  public static final String BARCODE_TYPE_TAG = "barcodeType";
+  public static final String BARCODE_LENGTH_TAG = "length";
+  public static final String DECODE_DATA_TAG = "barcode";
+
+  private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+      String action = intent.getAction();
+      Log.i(TAG,"onReceive , action:" + action);
+      // Get Scan Image . Make sure to make a request before getting a scanned image
+      if (ACTION_DECODE.equals(action)) {
+        // Get scan results, including string and byte data etc.
+        byte[] barcode = intent.getByteArrayExtra(DECODE_DATA_TAG);
+        int barcodeLen = intent.getIntExtra(BARCODE_LENGTH_TAG, 0);
+        byte temp = intent.getByteExtra(BARCODE_TYPE_TAG, (byte) 0);
+        String barcodeStr = intent.getStringExtra(BARCODE_STRING_TAG);
+        Log.i(TAG,"barcode type:" + temp);
+        String scanResult = new String(barcode, 0, barcodeLen);
+        // print scan results.
+        scanResult = " length：" + barcodeLen + "\nbarcode：" + scanResult  + "\nbarcodeStr:" + barcodeStr;
+
+        PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, scanResult);
+        pluginResult.setKeepCallback(true);
+        callbackContext.sendPluginResult(pluginResult);
+      }
+    }
+  };
 
   private CallbackContext callbackContext;
 
@@ -23,7 +61,7 @@ public class WMS extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
       this.callbackContext = callbackContext;
         if (action.equals("coolMethod")) {
-//             String message = args.getString(0);
+            String message = args.getString(0);
 //            this.coolMethod(message, callbackContext);
           Toast.makeText(cordova.getActivity(), callbackContext.toString(), Toast.LENGTH_SHORT).show();
             return true;
@@ -42,10 +80,21 @@ public class WMS extends CordovaPlugin {
   @Override
   public void onResume(boolean multitasking) {
     super.onResume(multitasking);
-    PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, "cordova: "+ UUID.randomUUID().toString());
+    PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, callbackContext.toString()+" cordova: "+ UUID.randomUUID().toString());
     pluginResult.setKeepCallback(true);
     callbackContext.sendPluginResult(pluginResult);
-//     Toast.makeText(cordova.getActivity(), "你好 cordova 回来了 :"+callbackContext.toString(), Toast.LENGTH_SHORT).show();
+    Toast.makeText(cordova.getActivity(), "你好 cordova 回来了 :"+callbackContext.toString(), Toast.LENGTH_SHORT).show();
+
+    // register broadcast
+    IntentFilter filter = new IntentFilter();
+    filter.addAction(ACTION_DECODE);
+    cordova.getContext().registerReceiver(mReceiver, filter);
+  }
+
+  @Override
+  public void onPause(boolean multitasking) {
+    super.onPause(multitasking);
+    cordova.getContext().unregisterReceiver(mReceiver);
 
   }
 }
